ccabd884fd2a43c9a3bd8ce2701473521f868d9c
syeda963 <139653670+syeda963@users.noreply.github.com>
2023-11-16 00:58:32 +0530

    Create allStationContactPerson.js

---
+++ allStationContactPerson.js
@@ -0,0 +1,626 @@
+let Mysore = [
+        {
+                name: "R. DEEPAK SSE / II / MYS",
+                phoneNumber: 9731667830
+        },
+        {
+                name: "V. Girish SSE / II / MYS",
+                phoneNumber: 7625067493
+        },
+        {
+                name: "H.K.MADHU SSE / III / MYS",
+                phoneNumber: 7625067588
+        },
+        {
+                name: "YOGESH ESM / CAB",
+                phoneNumber: 7676218269
+        },
+        {
+                name: "RAMKUMAR ESM / MYS",
+                phoneNumber: 7676218273
+        },
+        {
+                name: "Sai Kumar ESM / MYS",
+                phoneNumber: 7676218266
+        },
+        {
+                name: "LOHITHA ESM / MYS",
+                phoneNumber: 9741587286
+        },
+        {
+                name: "Dharamdev MCM / MYS",
+                phoneNumber: 7676218315
+        },
+        {
+                name: "DEVARAJ MCM / MYS",
+                phoneNumber: 7625067580
+        },
+        {
+                name: "SABIN MCM / MYS",
+                phoneNumber: 7676218259
+        },
+        {
+                name: "Harapriya ESM / MYS",
+                phoneNumber: 7676218279
+        },
+        {
+                name: "Pandiyan ESM / MYS",
+                phoneNumber: 7676218268
+        },
+        {
+                name: "Pushpa ESM / MBBC",
+                phoneNumber: 7676218282
+        },
+        {
+                name: "Nithin Kumar ESM / NHY",
+                phoneNumber: 7676218257
+        },
+        {
+                name: "Arun Kumar ESM / NHY",
+                phoneNumber: 7676218260
+        },
+        {
+                name: "NIKHIL ESM / PANP",
+                phoneNumber: 8157936440
+        },
+        {
+                name: "PUNITH ESM / PANP",
+                phoneNumber: 7625067585
+        },
+        {
+                name: "RASMITH ESM / MNGT",
+                phoneNumber: 7676218263
+        },
+        {
+                name: "Arun J Raj ESM / AP",
+                phoneNumber: 7676218275
+        },
+        {
+                name: "Arjun  ESM / AP",
+                phoneNumber: 7483086754
+        },
+        {
+                name: "Manoj Kumar ESM / KDO",
+                phoneNumber: 7676218314
+        },
+        {
+                name: "Suresh ESM / NTW",
+                phoneNumber: 7676218249
+        },
+        {
+                name: "Vinod ESM / NTW",
+                phoneNumber: 7625067589
+        },
+        {
+                name: "RAJENDRAN ESM / CMNR",
+                phoneNumber: 7676218274
+        },
+]
+
+let Holenarsipur = [
+        {
+                name: "Dineshram SSE / HLN", phoneNumber: 9731667821
+        },
+        {
+                name: "V. Chandru SSE / KRNR", phoneNumber: 7625067487
+        },
+        {
+                name: "Valrajan ESM / MNGT", phoneNumber: 9798345552
+        },
+        {
+                name: "Suhas ESM / BLGA", phoneNumber: 6360391840
+        },
+        {
+                name: "CHETHAN ESM / KRNR", phoneNumber: 7676218277
+        },
+        {
+                name: "SRINIVAS ESM / KRNR", phoneNumber: 7676218253
+        },
+        {
+                name: "YUSUF ALI ESM / KRNR", phoneNumber: 7676218258
+        },
+        {
+                name: "Ravinder ESM / AKK", phoneNumber: 7676218265
+        },
+        {
+                name: "SOMASHEKAR ESM / AKK", phoneNumber: 7676218284
+        },
+        {
+                name: "A.T. NAGRAJ MCM / HLN", phoneNumber: 7625067500
+        },
+        {
+                name: "Ashok  Gupta ESM / HLN", phoneNumber: 7676218256
+        },
+        {
+                name: "Sharuk ESM//HLN", phoneNumber: 6360392173
+        },
+        {
+                name: "Pappu ESM / HAS", phoneNumber: 9830754473
+        },
+]
+
+let Hassan = [
+        {
+                name: "Vijay Kumar SSE/I / HAS", phoneNumber: 9731667816
+        },
+        {
+                name: "Rajendran SSE/I / HAS", phoneNumber: 7259014339
+        },
+        {
+                name: "Arjun SSE/III/HAS", phoneNumber: 7259024785
+        },
+        {
+                name: "Kunaram SSE/SKLR", phoneNumber: 9731667822
+        },
+        {
+                name: "Anil Kumar MCM/HAS", phoneNumber: 7625067612
+        },
+        {
+                name: "Manojkumar MCM/HAS", phoneNumber: 9740212972
+        },
+        {
+                name: "Chandhan ESM/HAS", phoneNumber: 7676218321
+        },
+        {
+                name: "Suresh bhairav ESM/HAS", phoneNumber: 7676218226
+        },
+        {
+                name: "Balaram ESM/HAS", phoneNumber: 7676218335
+        },
+        {
+                name: "Venugopal ESM/HAS", phoneNumber: 7676218320
+        },
+        {
+                name: "Ranjay ESM/HAS", phoneNumber: 7676218319
+        },
+        {
+                name: "Punith ESM/HAS", phoneNumber: 6362706742
+        },
+        {
+                name: "Arjun kumar ESM/BGPA", phoneNumber: 7676218324
+        },
+        {
+                name: "Mohan MCM/ALUR", phoneNumber: 7625067497
+        },
+        {
+                name: "Raj kumar ESM/SKLR", phoneNumber: 6360379808
+        },
+        {
+                name: "Basil ESM/SKLR", phoneNumber: 6360391422
+        },
+        {
+                name: "Jishnu ESM/SKLR", phoneNumber: 8888888
+        },
+        {
+                name: "Mahesh ESM/SKLR", phoneNumber: 8888888
+        },
+        {
+                name: "Vaibhav Gupta ESM/SKLR", phoneNumber: 7676218330
+        },
+        {
+                name: "Bablu ESM/YDK", phoneNumber: 6360386379
+        },
+        {
+                name: "Kamal kr Meena ESM", phoneNumber: 8888888
+        },
+]
+
+let Kabakaputtur = [
+    {
+            name: "P Manoj kumar SSE/KBPR", phoneNumber: 9731667827
+    },
+    {
+            name: "Sunjith Kumar JE/KBPR", phoneNumber: 7625067615
+    },
+    {
+            name: "Lohith Kumar JE/SBHR", phoneNumber: 9148974806
+    },
+    {
+            name: "Jayprakas ESM/YDM", phoneNumber: 7625067937
+    },
+    {
+            name: "Srihari ESM/SBHR", phoneNumber: 9353106990
+    },
+    {
+            name: "Vidya ESM/SBHR", phoneNumber: 9353106991
+    },
+    {
+            name: "Kamalesh ESM/SBHR", phoneNumber: 7676218332
+    },
+    {
+            name: "Kumar MCM/KBHR", phoneNumber: 7625067498
+    },
+    {
+            name: "Umasankar Prasad ESM/KBPR", phoneNumber: 88888888
+    },
+    {
+            name: "Subhramaniya ESM/KBPR", phoneNumber: 7676218316
+    },
+    {
+            name: "Rahul Kumar ESM/KBPR", phoneNumber: 6360379581
+    },
+    {
+            name: "Anil Kumar ESM/BNTL", phoneNumber: 7878787887
+    },
+    {
+            name: "Prakash ESM/BNTL", phoneNumber: 77777777
+    }
+]
+
+let  Arsikere = [
+    {
+            name: "Sanjeev SSE/ASK", phoneNumber: 9731667824
+    },
+    {
+            name: "Koushuk A JE/I/ASK", phoneNumber: 9731667828
+    },
+    {
+            name: "Ranjith JE/I/ASK", phoneNumber: 7676218264
+    },
+    {
+            name: "Manish KR SSE/TTR", phoneNumber: 7259024775
+    },
+    {
+            name: "Ramaiah MCM/ASK", phoneNumber: 7625067590
+    },
+    {
+            name: "Sathyamurti MCM/ASK", phoneNumber: 7625067601
+    },
+    {
+            name: "Balaji MCM/ASK", phoneNumber: 7625067593
+    },
+    {
+            name: "Kundan ESM/ASK", phoneNumber: 6360387619
+    },
+    {
+            name: "Hansraj ESM/ASK", phoneNumber: 7676218304
+    },
+    {
+            name: "Wasim Akram ESM/ASK", phoneNumber: 7676218311
+    },
+    {
+            name: "Zuber ESM/ASK", phoneNumber: 1111111
+    },
+    {
+            name: "Lokesh ESM/ASK", phoneNumber: 655555555
+    },
+    {
+            name: "Ajay Kumar ESM/VNR", phoneNumber: 7676218308
+    },
+    {
+            name: "Kumar Swamy ESM/VNR", phoneNumber: 7676218518
+    },
+    {
+            name: "Ugranarayan ESM/VNR", phoneNumber: 6360381400
+    },
+    {
+            name: "Pankaj Kr ESM/VNR", phoneNumber: 6360383318
+    },
+    {
+            name: "Akhilesh kr MCM/TTR", phoneNumber: 7625067504
+    },
+    {
+            name: "Trinath Rao ESM/TTR", phoneNumber: 7676218235
+    },
+    {
+            name: "N Nagasuresh ESM/TTR", phoneNumber: 6360383616
+    },
+    {
+            name: "Akash ESM/AMSA", phoneNumber: 6360380783
+    },
+    {
+            name: "Ranjit ESM/AMSA", phoneNumber: 6360382506
+    },
+    {
+            name: "D S Meena ESM/AMSA", phoneNumber: 6360389063
+    }
+]
+
+let  Birur = [
+    {
+            name: "Sankarlal Sahu SSE/RRB", phoneNumber: 7259024776
+    },
+    {
+            name: "Mukesh JE/RRB", phoneNumber: 7676218306
+    },
+    {
+            name: "Karthik JE/SHV", phoneNumber: 7259014305
+    },
+    {
+            name: "Sudeep JE/DRU", phoneNumber: 7676218303
+    },
+    {
+            name: "D.Srinivas MCM/DRU", phoneNumber: 7625067605
+    },
+    {
+            name: "P Chaudhary MCM/DRU", phoneNumber: 7676218309
+    },
+    {
+            name: "Panduranga ESM/DRU", phoneNumber: 7676218310
+    },
+    {
+            name: "Prince Raj ESM/DRU", phoneNumber: 6202393661
+    },
+    {
+            name: "Abhishek ESM/CMGR", phoneNumber: 888888
+    },
+    {
+            name: "Ananda ESM/RRB", phoneNumber: 7676218230
+    },
+    {
+            name: "Kannan MCM/RRB", phoneNumber: 7625067596
+    },
+    {
+            name: "Halleshappa ESM/RRB", phoneNumber: 7625067592
+    },
+    {
+            name: "Govind Meena ESM/RRB", phoneNumber: 55555555
+    },
+    {
+            name: "Martin Jose ESM/RRB", phoneNumber: 77777777
+    },
+    {
+            name: "Dodde Gowda ESM/RRB", phoneNumber: 7777777
+    },
+    {
+            name: "Pandu ESM/AJP", phoneNumber: 7676218232
+    },
+    {
+            name: "Shyam kr ESM/AJP", phoneNumber: 6360382510
+    },
+    {
+            name: "Rajdev ESM/SHV", phoneNumber: 7676218224
+    },
+    {
+            name: "Sonu ESM/SHV", phoneNumber: 6360383281
+    },
+    {
+            name: "Purshotham ESM/TKE", phoneNumber: 6360382982
+    },
+    {
+            name: "Sandeep ESM/HSD", phoneNumber: 777777777
+    },
+    {
+            name: "Sajeev ESM/HSD", phoneNumber: 77777
+    },
+    {
+            name: "Ravi", phoneNumber: 8888888
+    }
+]
+
+let Shimoga  = [
+    {
+            name: "Bhavesh Kumar SSE/SMET", phoneNumber: 7259024773
+    },
+    {
+            name: "Shivkumar JE/SMET", phoneNumber: 7625067581
+    },
+    {
+            name: "Maruthi ESM/TKE", phoneNumber: 6360390574
+    },
+    {
+            name: "Purushatham kr ESM/TKE", phoneNumber: 6360382982
+    },
+    {
+            name: "Basavaraj MCM/BDVT", phoneNumber: 7625067599
+    },
+    {
+            name: "Wilson ESM/BDVT", phoneNumber: 7676218227
+    },
+    {
+            name: "Harish kr MCM/SMET", phoneNumber: 7625067600
+    },
+    {
+            name: "Jalil ESM/SMET", phoneNumber: 7676218228
+    },
+    {
+            name: "Prasanna ESM/SMET", phoneNumber: 7676218292
+    },
+    {
+            name: "Umesh N ESM/II/SMET", phoneNumber: 7676218237
+    },
+    {
+            name: "Suresh ESM/SMET", phoneNumber: 88888888
+    },
+    {
+            name: "Ranganatha ESM/SRF", phoneNumber: 7676218229
+    },
+    {
+            name: "Raghu nanda MCM/SRF", phoneNumber: 7625067611
+    },
+    {
+            name: "Srikant ESM/ANF", phoneNumber: 7676218291
+    },
+    {
+            name: "Vinayak ESM/ANF", phoneNumber: 9739566847
+    },
+    {
+            name: "Kiran ESM/TLGP", phoneNumber: 6360393271
+    }
+]
+
+let  Chikjajur= [
+    {
+            name: "Venkat suresh SSE/I/JRU", phoneNumber: 7625067488
+    },
+    {
+            name: "N M Swamy SSE/SPL", phoneNumber: 7259014337
+    },
+    {
+            name: "Ramesh SSE/DVG", phoneNumber: 7259024772
+    },
+    {
+            name: "Ballu Deva JE/II/JRU", phoneNumber: 7676218302
+    },
+    {
+            name: "Kumar ESM/RGI", phoneNumber: 9880975077
+    },
+    {
+            name: "Thimmappa MCM/HLK", phoneNumber: 7625067604
+    },
+    {
+            name: "Basavaraj ESM/HLK", phoneNumber: 7676218223
+    },
+    {
+            name: "Pintu ESM/JRU", phoneNumber: 6360388402
+    },
+    {
+            name: "Sakaldeep ESM/JRU", phoneNumber: 7676218301
+    },
+    {
+            name: "Rudresh ESM/JRU", phoneNumber: 7676218238
+    },
+    {
+            name: "Sudhir ESM/JRU", phoneNumber: 6360382593
+    },
+    {
+            name: "Ramappa ESM/JRU", phoneNumber: 7676218299
+    },
+    {
+            name: "Ekka ESM/MYK", phoneNumber: 7676218247
+    },
+    {
+            name: "Kallu Bairwa ESM/SLU", phoneNumber: 6360385215
+    },
+    {
+            name: "Nagmani Tiwari ESM/MYK", phoneNumber: 6360381022
+    },
+    {
+            name: "Neetesh ESM/MYK", phoneNumber: 6360381428
+    },
+    {
+            name: "Jitendra ESM/THN", phoneNumber: 8888888
+    },
+    {
+            name: "Ramachandra ESM/THN", phoneNumber: 7625067933
+    },
+    {
+            name: "L Rajappa MCM/DVG", phoneNumber: 7625067595
+    },
+    {
+            name: "J Anil kumar MCM/DVG", phoneNumber: 7625067614
+    }
+]
+
+let  Chitradurga= [
+    {
+            name: "Ravi narayan SSE/CTA", phoneNumber: 9731667821
+    },
+    {
+            name: "Anil kumar SSE/CHKE", phoneNumber: 7625067613
+    },
+    {
+            name: "Randheer sing ESM/III/AMC", phoneNumber: 7777777
+    },
+    {
+            name: "ManoJ ESM/AMC", phoneNumber: 6360389487
+    },
+    {
+            name: "Kalleshappa MCM/CTA", phoneNumber: 7625067607
+    },
+    {
+            name: "Nagaraj ESM/CTA", phoneNumber: 7625067934
+    },
+    {
+            name: "Thimmappa ESM/CTA", phoneNumber: 7676218234
+    },
+    {
+            name: "Joseph ESM/CTA", phoneNumber: 77777
+    },
+    {
+            name: "Manish ESM/III/CTA", phoneNumber: 88888
+    },
+    {
+            name: "Pramod MCM/THKU", phoneNumber: 7676218245
+    },
+    {
+            name: "Rajesh meena ESM/CHKE", phoneNumber: 77777
+    },
+    {
+            name: "Sanna kamaiya ESM/THKU", phoneNumber: 7676218248
+    },
+    {
+            name: "Mohammed illiyas ESM/MOMU", phoneNumber: 7676218251
+    },
+    {
+            name: "Monu jha ESM/III/MOMU", phoneNumber: 7625067594
+    }
+]
+
+let  Harihar= [
+    {
+            name: "Madhu N SSE/HRR", phoneNumber: 7625067484
+    },
+    {
+            name: "B N Ravi SSE/II/HRR", phoneNumber: 7625067490
+    },
+    {
+            name: "Pawan Kumar JE/HRR", phoneNumber: 7659014340
+    },
+    {
+            name: "Radhakrishna SSE/RNR", phoneNumber: 7625067485
+    },
+    {
+            name: "Manjappa ESM/II/TLGI", phoneNumber: 7676218240
+    },
+    {
+            name: "Halappa ESM/II/HPHI", phoneNumber: 7676218242
+    },
+    {
+            name: "Shubham kr ESM/III/AVC", phoneNumber: 7407036921
+    },
+    {
+            name: "M Yusuf MCM/HRR", phoneNumber: 7625067602
+    },
+    {
+            name: "Rinku kr meena MCM/HRR", phoneNumber: 7625067583
+    },
+    {
+            name: "Sikandar pasha MCM/HRR", phoneNumber: 7676218286
+    },
+    {
+            name: "Kiran kumar ESM/I/HRR", phoneNumber: 9353110159
+    },
+    {
+            name: "D Manjappa /ESM/HRR", phoneNumber: 888888
+    },
+    {
+            name: "M Divakar ESM/III/HRR", phoneNumber: 9035959355
+    },
+    {
+            name: "Thaggin annapp MCM/CLI", phoneNumber: 7625067603
+    },
+    {
+            name: "Arun s kumar ESM/II/CLI", phoneNumber: 7676218288
+    },
+    {
+            name: "Basava raju MCM/RNR", phoneNumber: 7625067609
+    },
+    {
+            name: "H K Raja MCM/RNR", phoneNumber: 7676218389
+    },
+    {
+            name: "Ravi kumar ESM/DAD", phoneNumber: 7676218296
+    },
+    {
+            name: "D B Arali MCM/HVR", phoneNumber: 7625067608
+    },
+    {
+            name: "Basavanappa ESM/III/HVR", phoneNumber: 7777
+    },
+    {
+            name: "Jamal sab T S MCM/HVR", phoneNumber: 7625067502
+    },
+    {
+            name: "Jagadish Pujari ESM/I/HVR", phoneNumber: 7676218298
+    },
+    {
+            name: "Nagaraj Badigar ESM/I/HVR", phoneNumber: 7676218295
+    },
+    {
+            name: "Laxmi salera ESM/HVR", phoneNumber: 8105441822
+    },
+    {
+            name: "Prasant katler ESM/HVR", phoneNumber: 77777
+    }
+]
